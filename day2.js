73424c9dd62cb082cf97ce2feb89ccd6966524f9
shaileshkumar <856shailesh@gmail.com>
2024-11-06 15:50:36 +0530

    end of day2

---
+++ day2.js
@@ -0,0 +1,19 @@
+Fullname = "Shailesh Kumar"
+console.log(typeof(Fullname)) //asking what type of data is FullName
+
+Fullname = 78
+console.log(typeof(Fullname))
+
+Fullname = true
+console.log(typeof(Fullname))
+
+//secondName = undefined;
+//secondName
+//console.log(typeof(secondName)); //error
+
+thirdname = null
+console.log(typeof(thirdname)); // Object (but this is primitve)
+//why this is returning Object (non primitive) , this is a bug
+
+String = "shailesh";
+console.log(String);
