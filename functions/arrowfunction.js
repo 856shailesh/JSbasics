5d304c7039087fbd226987be5a904053bbf2dafc
shaileshkumar <856shailesh@gmail.com>
2024-11-12 18:28:55 +0530

    end of day6

---
+++ functions/arrowfunction.js
@@ -0,0 +1,6 @@
+
+const sum = (a,b) => {
+    let c = a+b;
+    return c;
+}
+console.log(sum(2,3));
---
+++ functions/function1.js
@@ -0,0 +1,7 @@
+function sum ( a , b){
+    let c = a+b
+    //console.log(`The sum of ${a} and ${b} is ` + c )
+    return c 
+}
+const c = sum(2,4) //function calling
+console.log(`Sum result is ${c}`)
