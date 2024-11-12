48c294451930154c91e4acad20382706755b3776
shaileshkumar <856shailesh@gmail.com>
2024-11-12 08:31:25 +0530

    day 5 complete

---
+++ loops/KindofForLoops.js
@@ -0,0 +1,9 @@
+let marks = [24,23,67,34,86];
+console.log(marks)
+//print every element one by one
+//1. classsic for loop
+//for(i = 0 ; i < marks.length ;i++)
+    //console.log(marks[i])
+//2. For of Loop : works on value directly 
+for(const i of marks)
+    console.log(i)
---
+++ loops/forLoop.js
@@ -0,0 +1,11 @@
+// initialization Block ; Conditional blk ; Incremental Block
+// comes out of block , when condition is false
+for(let i = 0 ; i <= 1 ; i++){
+    console.log("Shailesh Kumar " + i )
+}
+
+let j = 1;
+while (j <= 3) {
+    console.log("While loop "+j)
+    j++;
+}
