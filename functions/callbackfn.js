00a1e119330dc999a0e8a28a5b758930fe90822c
shaileshkumar <856shailesh@gmail.com>
2024-11-14 19:10:07 +0530

    end of day8

---
+++ functions/callbackfn.js
@@ -0,0 +1,10 @@
+//currently it's upto calculate whether to call sum or not
+//if it doens't call sum , it will not execute even if we have
+//passed sum as a parameter.
+function caluclate(a,b , fn){
+    //fn(a,b); 
+}
+function sum(a,b){
+    return a+b;
+}
+caluclate(2,3,sum);
