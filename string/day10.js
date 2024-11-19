e4b49a894f99ae95edd1bf7c0e4b90f0dd5684c9
shaileshkumar <856shailesh@gmail.com>
2024-11-19 19:04:09 +0530

    end of day 10

---
+++ string/day10.js
@@ -0,0 +1,15 @@
+/*
+const firstName = "Shailesh";
+const extract = firstName.slice(0, 3) //First 3 char [inc , excluded]
+console.log(firstName.length-8)
+
+const sliceExtract = firstName.slice(0, 3)
+console.log(sliceExtract)
+//Difference , -ve values allowed in Slice (Traverse from end)
+const negative = firstName.slice(-4)
+console.log(negative) */
+
+//Count the words in a sentence
+const sentence = "My name is shailesh kumar"
+const words = sentence.split(" ")
+console.log(words)
